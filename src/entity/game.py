0a381af70bf5dce400b6dc19eb95ea7cb163215c
Richard Binder <binder.richard@outlook.de>
2024-10-15 00:10:09 +0200

    Fix click on non-empty square

--- src/entity/game.py
+++ src/entity/game.py
@@ -13,13 +13,14 @@ class Game:
         self.game_render = game_render
 
     def move(self, square):
-        self.board.move(square)
-        square.square_render.update_render()
-        self.game_render.play_move_sound()
-        if not self.board.state.has_ended:
-            position = self.ai.best_move()
-            self.board.move(position)
-            position.square_render.update_render()
+        if square.is_empty():
+            self.board.move(square)
+            square.square_render.update_render()
             self.game_render.play_move_sound()
-        if self.board.state.has_ended:
-            self.game_render.play_game_end_sound()
+            if not self.board.state.has_ended:
+                position = self.ai.best_move()
+                self.board.move(position)
+                position.square_render.update_render()
+                self.game_render.play_move_sound()
+            if self.board.state.has_ended:
+                self.game_render.play_game_end_sound()
